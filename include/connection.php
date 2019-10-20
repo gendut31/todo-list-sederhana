8f97ef020bfb0309963df706ef77ce7eb81cce2d
Tri Cahyo Gusmantoro <gusmangendut@gmail.com>
2019-10-20 11:44:09 +0800

    add connection file

---
+++ include/connection.php
@@ -0,0 +1,22 @@
+<?php
+
+class DbConnect{
+
+    protected $conn;
+
+    function __construct(){
+        date_default_timezone_set("Asia/Kuala_Lumpur");
+
+        include '../config/config.php';
+
+        $this->conn = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+        if (mysqli_connect_errno()) {
+            echo "Koneksi gagal";
+        } else {
+            echo "Koneksi sukses";
+        }
+
+        return $this->conn;
+    }
+}
